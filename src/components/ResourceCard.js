2a9bf1f3654e1504c87e9c70ff6b3dbbb4ec9282
armans-code <arman.kumaraswamy@gmail.com>
2022-08-12 22:04:11 -0400

    Added bylaws, outreach letter + tracker resources

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -1,21 +1,40 @@
-import React from 'react';
-
 function ResourceCard(props) {
 	return (
 		<article class='overflow-hidden rounded-lg shadow-lg items-center flex flex-col justify-center gap-2 p-4'>
 			<h1 class='text-lg'>
-				<a class='no-underline hover:underline text-black' href='#'>
-					{props.name}
-				</a>
+				{props.location ? (
+					<a
+						class='no-underline hover:underline text-black'
+						href={props.location}
+						download
+					>
+						{props.name}
+					</a>
+				) : (
+					<a
+						class='no-underline hover:underline text-black'
+						href={props.link}
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						{props.name}
+					</a>
+				)}
 			</h1>
-			<a
-				href={props.link}
-				target='_blank'
-				rel='noopener noreferrer'
-				className={`text-pink-500 underline`}
-			>
-				{props.link}
-			</a>
+			{props.location ? (
+				<a className={`text-pink-500 underline`} href={props.location} download>
+					{props.link}
+				</a>
+			) : (
+				<a
+					href={props.link}
+					target='_blank'
+					rel='noopener noreferrer'
+					className={`text-pink-500 underline`}
+				>
+					{props.linkname}
+				</a>
+			)}
 			<p className='text-sm text-gray-600'>{props.desc}</p>
 		</article>
 	);
--- src/pages/ResourcesPage.js
+++ src/pages/ResourcesPage.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import ResourceCard from '../components/ResourceCard';
+import bylaws from '../resources/SADD Club By-Laws LOLHS.pdf';
+import letter from '../resources/LOLHS SADD Sponsor Letter.pdf';
 
 function ResourcesPage() {
 	return (
 		<div className='flex flex-col h-screen items-center justify-between'>
-			<div class='mt-24 px-1 w-full md:w-1/2 lg:px-4 lg:w-1/3'>
+			<div class='mt-24 px-1 flex-col flex gap-20 items-center justify-around'>
 				<ResourceCard
-					name='Official SADD website'
+					name='Official SADD Website'
 					desc='This is the official SADD website, where you can find information
 					about the SADD mission, visions, events, etc.'
 					link='https://www.sadd.org/'
+					linkname='https://www.sadd.org/'
+				/>
+				<ResourceCard
+					name='LOLHS SADD Club By-Laws'
+					desc="This is our school chapter's By-laws, which explains the rules by which our club operates."
+					link='Click here to download By-laws.'
+					location={bylaws}
+				/>
+				<ResourceCard
+					name='LOLHS SADD Sponsor Letter Template'
+					desc="This is our chapter's letter meant to be sent to local businesses in search of sponsors."
+					link='Click here to download Outreach Letter.'
+					location={letter}
+				/>
+				<ResourceCard
+					name='SADD Outreach Tracker'
+					desc="This is our chapter's Outreach Tracker form, used to track our sponsors."
+					link='https://forms.office.com/Pages/ResponsePage.aspx?id=aaJT4khSVE6bZ7duOoYiIGFiPVvxEt5Hsu8-1FmQxa5UN0VRWENFUkQ3SFFUMzkwRERKQk9JRzAwSS4u'
+					linkname='Link to Outreach Tracker Form'
 				/>
 			</div>
 			<Footer />
